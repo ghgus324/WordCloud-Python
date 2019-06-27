9731c2d74d772003ac9d71c72e4e97af9c5ee483
kimhohyeon <ghgus323@naver.com>
2019-06-27 22:06:19 +0900

    word_clould.py update

--- word_clould.py
+++ word_clould.py
@@ -17,15 +17,21 @@ import os
 app = Flask(__name__, static_folder='outputs')
 CORS(app)
 
-# 폰트 경로 설정
+#폰트 경로 설정
 font_path = 'NanumGothic.ttf'
 
 
-def get_tags(text):
+def get_tags(text, max_count, min_length):
     t = Twitter()
     nouns = t.nouns(text)
-    count = Counter(nouns)
-    return count
+    processed = [n for n in nouns if len(n) >= min_length]
+    count = Counter(processed)
+    result = {}
+    for n, c in count.most_common(max_count):
+        result[n] = c
+    if len(result) == 0:
+        result["내용이 없습니다."] = 1
+    return result
 
 
 def make_cloud_image(tags, file_name):
@@ -43,22 +49,46 @@ def make_cloud_image(tags, file_name):
     # 만들어진 이미지 객체를 파일 형태로 저장합니다.
     fig.savefig("outputs/{0}.png".format(file_name))
 
-def process_from_text(text):
-    tags = get_tags(text)
-    make_cloud_image(tags,"output")
+
+def process_from_text(text, max_count, min_length, words, file_name):
+     tags = get_tags(text, int(max_count), int(min_length))
+    # 단어 가중치를 적용합니다.
+    for n, c in words.items():
+        if n in tags:
+            tags[n] = tags[n] * float(words[n])
+    make_cloud_image(1, file_name)
+
 
 @app.route("/process", methods=['GET', 'POST'])
 def process():
-    # process에 접속 됬을때 사용자에게 request.json을 받아서
-        content = request.json
-        words = {}
-        if content['words'] is not None:
-            for data in content['words'].values():
-                words[data['word']] = data['weight'] #단어 내용에 따라 가중치 정보를 넣는다
-        #사용자가 text를 보내면 처리할 수 있도록 메소드 만듬
-        process_from_text(content['text'])
-        return jsonify(words)
+    content = request.json
+    words = {}
+    if content['words'] is not None:
+        for data in content['words'].values():
+            words[data['word']] = data['weight']
+    process_from_text(content['text'], content['maxCount'], content['minLength'], words, content['textID'])
+    result = {'result': True}
+    return jsonify(result)
 
-# 실제로 프로그램이 시작 되었을때 플라스크로 작성된 이 웹서버가 정상적으로 작동하면 됨
+
+@app.route('/outputs', methods=['GET', 'POST'])
+def output():
+    text_id = request.args.get('textID')
+    return app.send_static_file(text_id + '.png')
+
+
+@app.route('/validate', methods=['GET', 'POST'])
+def validate():
+    text_id = request.args.get('textID')
+    path = "outputs/{0}.png".format(text_id)
+    result = {}
+    # 해당 이미지 파일을 존재하는지 확인합니다.
+    if os.path.isfile(path):
+        result['result'] = True
+    else:
+        result['result'] = False
+    return jsonify(result)
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000)
+    app.run('0.0.0.0', port=5000, threaded=True)
